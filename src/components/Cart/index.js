e97f41b3d79600bff2cd37c43a0935ada2b525bb
farazsfa007 <farazahmadsfa@gmail.com>
2025-01-10 17:31:19 +0000

    first commit for nxt trends app

---
+++ src/components/Cart/index.js
@@ -0,0 +1,45 @@
+import Header from '../Header'
+import CartListView from '../CartListView'
+
+import CartContext from '../../context/CartContext'
+import EmptyCartView from '../EmptyCartView'
+import CartSummary from '../CartSummary'
+
+import './index.css'
+
+const Cart = () => (
+  <CartContext.Consumer>
+    {value => {
+      const {cartList, removeAllCartItems} = value
+      const showEmptyView = cartList.length === 0
+      // TODO: Update the functionality to remove all the items in the cart
+      const onClickRemoveBtn = () => {
+        removeAllCartItems()
+      }
+      return (
+        <>
+          <Header />
+          <div className="cart-container">
+            {showEmptyView ? (
+              <EmptyCartView />
+            ) : (
+              <div className="cart-content-container">
+                <h1 className="cart-heading">My Cart</h1>
+                <button
+                  onClick={onClickRemoveBtn}
+                  type="button"
+                  className="remove-btn"
+                >
+                  Remove All
+                </button>
+                <CartListView />
+                <CartSummary />
+              </div>
+            )}
+          </div>
+        </>
+      )
+    }}
+  </CartContext.Consumer>
+)
+export default Cart
